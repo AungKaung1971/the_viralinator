62fb1d492a4e050b3df90064f490fd24690e2144
Aung Kaung <aungkaung855@gmail.com>
2025-11-10 20:07:00 +0000

    scraping is finally working

--- config.py
+++ config.py
@@ -1,7 +1,17 @@
+
+
 import os
 from dotenv import load_dotenv
+from pathlib import Path
+
 
 load_dotenv()
 
+env_path = Path(__file__).resolve().parent / ".env"
+load_dotenv(dotenv_path=env_path)
+
 openAI_key = os.getenv("OPENAI_API_KEY")
 data_base_path = "data/viralinator.db"
+
+auth_token = os.getenv("AUTH_TOKEN")
+cto_token = os.getenv("CTO_TOKEN")
--- monitor/influencer_monitor.py
+++ monitor/influencer_monitor.py
@@ -5,25 +5,46 @@ from pathlib import Path
 from datetime import datetime, timezone
 import logging
 from typing import List, Dict, Optional
-import snscrape.modules.twitter as sntwitter
+from twikit import Client
+from config import auth_token, cto_token
+import asyncio
 
 
-def get_usernames():
-    print("Enter Target Usernames: ")
+# def get_usernames():
+#     print("Enter Target Usernames: ")
 
-    usernames = []
-    while True:
-        username = input("> ").strip()
-        if username == "done":
-            break
-        elif username:
-            usernames.append(username)
+#     usernames = []
+#     while True:
+#         username = input("> ").strip()
+#         if username == "done":
+#             break
+#         elif username:
+#             usernames.append(username)
 
-    return usernames
+#     return usernames
+
+
+# if __name__ == "__main__":
+#     target_usernames = get_usernames()
+#     print("Usernames chosen:", target_usernames)
+
+async def main():
+    client = Client('en-US')
+    client.set_cookies({
+        'auth_token': '3ae4b197c808f88ee4a00a23f492041fa9f04c59',
+        'ct0': '798e8760ae05b91531994af462df378e843ccd085e5dfec67169146cef1f2086396732dbb3d0cae708cd70bcaec636c6dbcdbf227021a5dcb523e25090219016bec1aaca2c046519ba9ea2b23c1d8f47'
+    })
+
+    user = await client.get_user_by_screen_name('elonmusk')
+    tweets = await client.get_user_tweets(user.id, 'Tweets')
+
+    for t in tweets[:2]:
+        print(t.text)
 
 
 if __name__ == "__main__":
-    target_usernames = get_usernames()
-    print("Usernames chosen:", target_usernames)
+    asyncio.run(main())
 
-sntwitter.TwitterSearchScraper('from:')
+
+# rm -rf **/__pycache__/
+# python -m monitor.influencer_monitor
